e6672514b56159fd15312fa32da9d65090ce1ac0
laetitia <laetitia.saidi.sio@gmail.com>
2025-04-08 12:38:03 +0200

    Ajout des nouvelles fonctionnalités et modification des fichiers

---
+++ app/src/main/java/com/example/comeat/ListeRepasActivity.kt
@@ -0,0 +1,25 @@
+package com.example.comeat
+
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+
+//class ListeRepasActivity {
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val specialiteRepas = intent.getStringExtra("specialite_repas")
+//
+//
+//        val dateRepas = LocalDate.parse(intent.getStringExtra("date_repas"))
+//
+//
+//    }
+//}
+
+
+
+
